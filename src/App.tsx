079906b52bd3130461d8ccc8ff5b4dcbe5ad6958
notauserx <notauserx@users.noreply.github.com>
2022-11-23 21:22:11 +0600

    add event handler

--- src/App.tsx
+++ src/App.tsx
@@ -52,12 +52,23 @@ const Banner = () => (
   </div>
 )
 
-const Search = () => (
-  <div>
-    <label htmlFor="search">Search </label>
-    <input id="search" type="text" />
-  </div>
-);
+const Search = () => {
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    
+    console.log(event);
+
+    console.log(event.target.value);
+
+  }
+
+  return (
+    <div>
+      <label htmlFor="search">Search </label>
+      <input id="search" type="text" onChange={handleChange} />
+    </div>
+  );
+}
 
 
 const List = () => (
