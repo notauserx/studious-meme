1cc7c3d41eee926cef0bc3f937215d5351a240c4
notauserx <notauserx@users.noreply.github.com>
2022-11-29 16:13:26 +0600

    styling the search form

--- src/components/StoriesContainer/searchForm.tsx
+++ src/components/StoriesContainer/searchForm.tsx
@@ -1,8 +1,8 @@
 import { ChangeEvent, FC, FormEvent, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 
-const SearchForm = ({
-searchTerm,
+const SearchFormOld = ({
+  searchTerm,
   onSearchInput,
   onSearchSubmit,
 }: {
@@ -10,13 +10,57 @@ searchTerm,
   onSearchInput: (event: ChangeEvent<HTMLInputElement>) => void;
   onSearchSubmit: (event: FormEvent<HTMLFormElement>) => void;
 }) => (
-    <div className="">
-      <Search searchTerm={searchTerm} onSearchInput={onSearchInput} onSearchSubmit={onSearchSubmit} />
-      
+  <div className="">
+    <Search
+      searchTerm={searchTerm}
+      onSearchInput={onSearchInput}
+      onSearchSubmit={onSearchSubmit}
+    />
+  </div>
+);
+
+const SearchForm = ({
+  searchTerm,
+  onSearchInput,
+  onSearchSubmit,
+}: {
+  searchTerm: string;
+  onSearchInput: (event: ChangeEvent<HTMLInputElement>) => void;
+  onSearchSubmit: (event: FormEvent<HTMLFormElement>) => void;
+}) => (
+  <div >
+    <div className="w-full max-w-lg px-10 py-8 mx-auto bg-white dark:bg-gray-900 dark:text-white rounded-lg shadow-xl">
+      <div className="max-w-md mx-auto space-y-6">
+        <form onSubmit={onSearchSubmit}>
+          <h2 className="text-2xl font-bold ">Search</h2>
+          <p className="my-4 opacity-70">
+            Looking for anything specific
+          </p>
+          <hr className="my-6" />
+
+          <label className="uppercase text-sm font-bold opacity-70">
+            Query
+          </label>
+          <input
+            type="text"
+            className="p-3 mt-2 mb-4 w-full bg-white dark:bg-gray-900 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none"
+            onChange={onSearchInput}
+            value={searchTerm}
+          />
+
+          <TagSelect tag="" />
+
+          <button
+            type="submit"
+            className="py-3 px-6 my-2 bg-emerald-500 text-white font-medium rounded hover:bg-indigo-500 cursor-pointer ease-in-out duration-300"
+          >
+            Search
+          </button>
+        </form>
+      </div>
     </div>
-  );
-
-
+  </div>
+);
 
 const Search = ({
   searchTerm,
@@ -34,25 +78,25 @@ const Search = ({
       </label>
       <div className="flex">
         <input
-        className="appearance-none bg-transparent text-gray-700 dark:text-gray-200 mr-3 py-1 px-2 leading-tight focus:outline-none"
-        type="text"
-        placeholder="search"
-        value={searchTerm}
-        onChange={onSearchInput}
-      />
-      <FaSearch size="18" />
+          className="appearance-none bg-transparent text-gray-700 dark:text-gray-200 mr-3 py-1 px-2 leading-tight focus:outline-none"
+          type="text"
+          placeholder="search"
+          value={searchTerm}
+          onChange={onSearchInput}
+        />
+        <FaSearch size="18" />
       </div>
     </div>
 
-    <TagSelect tag="" />    
+    <TagSelect tag="" />
 
-<div className="w-full px-3 mb-6 md:mb-0">
-    <button
-      className="flex-shrink-0 bg-teal-500 hover:bg-teal-800 text-sm text-white py-2 px-2 rounded mt-4"
-      type="submit"
-    >
-      Search
-    </button>
+    <div className="w-full px-3 mb-6 md:mb-0">
+      <button
+        className="flex-shrink-0 bg-teal-500 hover:bg-teal-800 text-sm text-white py-2 px-2 rounded mt-4"
+        type="submit"
+      >
+        Search
+      </button>
     </div>
   </form>
 );
@@ -69,33 +113,22 @@ const TagSelect: FC<tagSelectProps> = ({ tag }) => {
   };
 
   return (
-    <div className="w-full px-3 mb-6 md:mb-0">
+    <>
+      <label className="uppercase text-sm font-bold opacity-70">Tags</label>
+      <select
+        className="w-full p-3 mt-2 mb-4 w-full bg-white dark:bg-gray-900 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none"
+        onChange={(event) => changeTag(event.target.value)}
+        value={searchTag}
+      >
+        <option value="">All</option>
+        <option value="story">Story</option>
+        <option value="poll">Pnll</option>
+        <option value="comments">Comments</option>
+      </select>
       <label className="block uppercase tracking-wide text-gray-400 text-xs font-bold mb-2">
         Tags
       </label>
-      <div className="relative">
-        <select
-          className="block appearance-none w-full bg-gray-200 dark:bg-gray-700 border border-gray-200 text-gray-700 dark:text-gray-200 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-          onChange={(event) => changeTag(event.target.value)}
-          value={searchTag}
-        >
-          <option value="">None</option>
-          <option value="story">Story</option>
-          <option value="poll">Poll</option>
-          <option value="comments">Comments</option>
-        </select>
-        
-        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-          <svg
-            className="fill-current h-4 w-4"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-          >
-            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-          </svg>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
